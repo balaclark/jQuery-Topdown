340a0064b4ffc6e9f156350321d20c576bb02a87
Bala Clark <nandasunu@gmail.com>
2010-06-07 09:33:58 +0100

    made the default life a little longer

--- jquery.topdown.js
+++ jquery.topdown.js
@@ -6,13 +6,13 @@
   Copyright 2010 Bala Clark
   www.balaclark.com
 
-  Licensed under the Apache License, Version 2.0 (the “License”);
+  Licensed under the Apache License, Version 2.0 (the ï¿½Licenseï¿½);
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
 
   http://www.apache.org/licenses/LICENSE-2.0
   Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an “AS IS” BASIS,
+  distributed under the License is distributed on an ï¿½AS ISï¿½ BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
@@ -51,7 +51,7 @@
 			style: "notification",
 			autoWidth: false,
 			speed: 200,
-			life: 2000,
+			life: 3000,
 			sticky: false,
 			url: false,
 			container_id: "td_container",
